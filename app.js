4da77926a16169ac9c255d62e6bd8eb46c47ae29
kanomdook <kanung.2535@gmail.com>
2018-08-31 21:25:28 +0700

    [edit] path

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 const port = process.env.PORT || '3000';
